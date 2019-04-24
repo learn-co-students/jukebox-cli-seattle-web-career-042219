c7e39bdcfa3c15f1b1c2cf05d54a4cb4e7680566
github username <github email address>
2019-04-24 13:59:59 -0700

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,72 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+my_songs = {
+ "Go Go GO" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/01.mp3',
+ "LiberTeens" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/02.mp3',
+ "Hamburg" =>  '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/03.mp3',
+ "Guiding Light" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/04.mp3',
+ "Wolf" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/05.mp3',
+ "Blue" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/07.mp3'
+ }
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index { |song, index|
+    puts (index + 1).to_s + ". " + song
+    }
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+  output = ""
+  songs.each_with_index { |song, index|
+    if user_response == (index + 1).to_s || user_response == song
+      output = "Playing #{song}"
+    end
+  }
+  if output.include?("Playing")
+    puts output
+  elsif user_response == "list"
+    list(songs)
+    play(songs)
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  while user_response != "exit"
+    case user_response
+      when "help"
+        help
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "list"
+        list(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "play"
+        play(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      else
+        puts "Invalid input, please try again"
+        user_response = gets.chomp
+    end
+  end
+  exit_jukebox
+end
